75f87772572d1140effc03aee10635adfa048ab7
Ben Best <ben@ecoquants.com>
2025-05-17 10:18:34 +0200

    cleaner process/qmd -> log/qmd |> render()

--- process/update_data.R
+++ process/update_data.R
@@ -13,7 +13,7 @@ librarian::shelf(
 dir_meta   <- here("meta")
 dir_log    <- here("log")
 log_txt    <- glue("{dir_log}/update_data_log.txt")
-do_git     <- TRUE
+do_git     <- F # DEBUG
 
 github_pat <- gitcreds_get(use_cache = FALSE)$password
 stopifnot(str_sub(github_pat, 1, 3) == "ghp")
@@ -25,40 +25,45 @@ if (file_exists(log_txt))
 log_appender(appender_tee(log_txt))
 log_tictoc("Script starting up...")
 
-for (yaml in dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")){ # yaml = dir_ls(dir_meta, glob = "*.yaml")[1]
+# for (yml in dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]){ # yml = dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]
+for (yml in dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]){ # DEBUG
 
   tryCatch({
 
     # parameters
-    params          <- read_yaml(yaml)
-    params$data_var <- path_ext_remove(basename(yaml))
+    params          <- read_yaml(yml)
+    params$data_var <- path_ext_remove(basename(yml))
 
     # paths
-    in_qmd   <- here("process/erddap.qmd")  # TODO: switch to copernicus based on params
-    tmp_html <- glue("{params$data_var}.html")
-    out_html <- here(glue("{dir_log}/{params$data_var}.html"))
+    proc_qmd <- here("process/erddap.qmd")  # TODO: switch to copernicus based on params
+    log_qmd  <- here("{dir_log}/{params$data_var}.qmd")
+    file_copy(proc_qmd, log_qmd, overwrite = T)
+
+    # tmp_html <- glue("{params$data_var}.html")
+    # out_html <- here(glue("{dir_log}/{tmp_html}"))
 
   }, error = function(e) {
-    log_error("Error reading {basename(yaml)}: {conditionMessage(e)}")
+    log_error("Error reading {basename(yml)}: {conditionMessage(e)}")
   })
 
-  log_tictoc("meta/{basename(yaml)} -[ {basename(in_qmd)} ]-> data/{params$data_var}/*, log/{basename(out_html)}")
+  log_tictoc("meta/{basename(yml)} -[ process/{basename(in_qmd)} ]-> data/{params$data_var}/*, log/{tmp_html}")
 
   tryCatch({
 
     quarto_render(
       input          = in_qmd,
-      output_format  = "html",
-      output_file    = tmp_html,
-      execute_params = params,
-      execute_dir    = dirname(in_qmd),
-      pandoc_args    = "--embed-resources",
-      debug          = T)
+      # output_format  = "html",
+      # output_file    = tmp_html,
+      execute_params = params)
+      # execute_dir    = dirname(in_qmd),
+      # pandoc_args    = "--embed-resources",
+      # debug          = T)
 
-    file_move(tmp_html, out_html)
+    # file_move(tmp_html, out_html)
+    file_delete(log_qmd)
 
   }, error = function(e) {
-    log_error("Error rendering {basename(yaml)} -[ {basename(in_qmd)} ]-> {basename(out_html)}: {conditionMessage(e)}")
+    log_error("Error rendering {basename(yml)} -[ {basename(in_qmd)} ]-> {basename(out_html)}: {conditionMessage(e)}")
   })
 
 }
