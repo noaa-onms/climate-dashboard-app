ba3b347865899c7ab534d8ce758a5098a950009f
Ben Best <ben@ecoquants.com>
2025-05-17 10:37:24 +0200

    fixed erddap.qmd -> log/var.qmd, var_files

--- process/update_data.R
+++ process/update_data.R
@@ -1,7 +1,8 @@
+# load packages ----
 librarian::shelf(
   dplyr, fs, gitcreds, glue, here, logger, quarto, stringr, yaml)
 
-# setup cron ----
+# setup cron
 # sudo apt update
 # sudo apt install cron
 # sudo systemctl enable cron
@@ -10,63 +11,63 @@ librarian::shelf(
 # 0 0 * * * cd /share/github/noaa-onms/climate-dashboard-app; Rscript process/update_data.R > log/update_data_cron.txt 2>&1
 # sudo service cron restart
 
+# paths ----
 dir_meta   <- here("meta")
 dir_log    <- here("log")
+dir_proc   <- here("process")
+dir_data   <- here("data")
 log_txt    <- glue("{dir_log}/update_data_log.txt")
 do_git     <- F # DEBUG
 
+# git ----
 github_pat <- gitcreds_get(use_cache = FALSE)$password
 stopifnot(str_sub(github_pat, 1, 3) == "ghp")
 if (do_git)
   system("git pull")
 
+# log ----
 if (file_exists(log_txt))
   file_delete(log_txt)
 log_appender(appender_tee(log_txt))
 log_tictoc("Script starting up...")
 
-# for (yml in dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]){ # yml = dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]
-for (yml in dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")[2]){ # DEBUG
+# iterate over dataset ymls ----
+ymls <- dir_ls(dir_meta, regexp = ".*\\.(ya?ml)$")
+for (yml in ymls){  # yml = ymls[2]
 
+  # * setup qmd ----
   tryCatch({
 
     # parameters
     params          <- read_yaml(yml)
     params$data_var <- path_ext_remove(basename(yml))
 
-    # paths
-    proc_qmd <- here("process/erddap.qmd")  # TODO: switch to copernicus based on params
-    log_qmd  <- here("{dir_log}/{params$data_var}.qmd")
+    # qmd
+    proc_qmd <- glue("{dir_proc}/erddap.qmd")  # TODO: switch to copernicus based on params
+    log_qmd  <- glue("{dir_log}/{params$data_var}.qmd")
     file_copy(proc_qmd, log_qmd, overwrite = T)
-
-    # tmp_html <- glue("{params$data_var}.html")
-    # out_html <- here(glue("{dir_log}/{tmp_html}"))
 
   }, error = function(e) {
     log_error("Error reading {basename(yml)}: {conditionMessage(e)}")
   })
 
-  log_tictoc("meta/{basename(yml)} -[ process/{basename(in_qmd)} ]-> data/{params$data_var}/*, log/{tmp_html}")
-
+  # * render qmd ----
+  log_tictoc("meta/{basename(yml)} -[ process/{basename(proc_qmd)} ]-> data/{params$data_var}/*, log/{params$data_var}.html")
   tryCatch({
 
     quarto_render(
-      input          = in_qmd,
-      # output_format  = "html",
-      # output_file    = tmp_html,
+      input          = log_qmd,
       execute_params = params)
-      # execute_dir    = dirname(in_qmd),
-      # pandoc_args    = "--embed-resources",
-      # debug          = T)
 
-    # file_move(tmp_html, out_html)
     file_delete(log_qmd)
 
   }, error = function(e) {
-    log_error("Error rendering {basename(yml)} -[ {basename(in_qmd)} ]-> {basename(out_html)}: {conditionMessage(e)}")
+    log_error("Error rendering {basename(yml)} -[ log/{basename(log_qmd)} ]-> log/{params$data_var}.html}: {conditionMessage(e)}")
   })
 
 }
+
+# git push ----
 log_tictoc("Script done rendering, next git commit & push")
 
 # setup in Terminal
